f9b3d1566bb20f0dd45c63ed5d2ba4db7fd5f2bd
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2026-03-09 13:21:13 +0500

    Add MySQL integration and dynamic/static page rendering examples in Next.js

---
+++ nextjs_app/app/(users)/static/page.jsx
@@ -0,0 +1,20 @@
+import { db } from "../../../config/db.jsx";
+
+const StaticPage = async () => {
+  const [doctors] = await db.execute("select * from doctors");
+  console.log(doctors);
+
+  return (
+    <>
+      <ul>
+        <li>
+          {doctors.map((doctor) => {
+            return <li key={doctor.doctor_id}>{doctor.first_name}</li>;
+          })}
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default StaticPage;
